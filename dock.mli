2d465ffd1f2a0034622618d40b5502cbc7531187
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-03 02:10:54 +0300

    Doc strings for the Dock module

--- dock.mli
+++ dock.mli
@@ -1,12 +1,25 @@
 (** Read/write to ROX {5,6}.0, Topline 2012 docking station *)
 
+(** Abstract type with docking station state. *)
 type t
 
+(** Close connection to the docking station. *)
 val close : t -> unit
+
+(** Open connection to the docking station through serial device file
+ * at [path]. *)
 val open_dock : string -> t
 
+(** Read [n] bytes of data from the docking station. *)
+val read : t -> int -> string
+
+(** Returns [true] if there's a cycling computer unit connected to the
+ * docking station. *)
 val unit_connected : t -> bool
+
+(** Model of cycling computer unit that is/was connected to the
+ * docking station. *)
 val unit_model : t -> Unit_model.t
 
-val read : t -> int -> string
+(** Write [data] to the docking station. *)
 val write : t -> string -> unit
