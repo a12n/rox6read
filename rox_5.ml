f9e8adabc2d94e607e7618ff5e5fd28b1650c3c1
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-04 19:08:00 +0300

    Rox_5.Totals: decode answer, fill record

--- rox_5.ml
+++ rox_5.ml
@@ -61,14 +61,47 @@ module Totals =
         hike_cal : int;
       }
 
-    let scan _ans =
-      { distance = (0, 0);
-        time = (0, 0);
-        cal = (0, 0);
-        climb = (0, 0);
-        hike_alt = 0;
-        hike_cal = 0;
-        hike_time = 0 }
+    let scan ans =
+      let c = char_codes ans in
+      { distance = (
+          (* Bike1 *)
+          (c.( 0) lor (c.( 1) lsl 8) lor (c.( 2) lsl 16) lor ((c.( 3) land 0x0F) lsl 24)) +
+            (((c.( 7) land 0xFC) lsl 2) lor ((c.( 3) land 0xF0) lsr 4)) / 1000
+        ,
+          (* Bike2 *)
+          (c.( 8) lor (c.( 9) lsl 8) lor (c.(10) lsl 16) lor ((c.(11) land 0x0F) lsl 24)) +
+            (((c.(15) land 0xFC) lsl 2) lor ((c.(11) land 0x0F) lsr 4)) / 1000
+        );
+        time = (
+          (* Bike1 *)
+          c.(4) lor (c.(5) lsl 8) lor (c.(6) lsl 16) lor (c.(7) land 0x03) lsl 24
+        ,
+          (* Bike2 *)
+          ((c.(15) land 3) lsl 24) lor (c.(14) lsl 16) lor (c.(13) lsl 8) lor c.(12)
+        );
+        cal = (
+          (* Bike1 *)
+          (((c.(25) land 0x04) lsr 2) lsl 16) lor (c.(17) lsl 8) lor c.(16)
+        ,
+          (* Bike2 *)
+          (((c.(25) land 0x08) lsr 3) lsl 16) lor (c.(19) lsl 8) lor c.(18)
+        );
+        climb = (
+          (* Bike1 *)
+          (((c.(28) land 0x0F) lsl 16) lor (c.(27) lsl 8) lor c.(26)) * 100 +
+            10 * ((c.(28) land 0xF0) lsr 4)
+        ,
+          (* Bike2 *)
+          (((c.(31) land 0x0F) lsl 16) lor (c.(30) lsl 8) lor c.(29)) * 100 +
+            10 * ((c.(31) land 0xF0) lsr 4)
+        );
+        hike_alt =
+          (((c.(34) land 15) lsl 16) land (c.(33) lsl 8) lor c.(32)) * 100 +
+            10 * ((c.(34) land 0xF0) lsr 4) ;
+        hike_cal =
+          (((c.(25) land 0x10) lsr 4) lsl 16) lor (c.(21) lsl 8) lor c.(20) ;
+        hike_time =
+          ((c.(25) land 0x03) lsl 24) lor (c.(24) lsl 16) lor (c.(23) lsl 8) lor c.(22) }
 
     let recieve =
       scan % command ~code:0xEF ~address:0x4200 ~ans_size:40
