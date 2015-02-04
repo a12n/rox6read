74eac699df448388fb3248a1049364cac837843f
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-05 01:17:25 +0300

    Add module for low battery level setting

---
+++ low_bat.ml
@@ -0,0 +1,9 @@
+(** Battery low if voltage in mV is below of the following *)
+type t = Below_1830
+       | Below_2000
+       | Below_2170
+       | Below_2330
+       | Below_2500
+       | Below_2670
+       | Below_2830
+       | Below_3000
