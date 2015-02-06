d0cab94a6d8ff7ecc8f32c82f598249465be0598
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-06 17:09:53 +0300

    Separate module for serial port I/O

---
+++ ser_port.ml
@@ -0,0 +1,45 @@
+type t = Unix.file_descr
+
+exception Timeout
+
+let cfmakeraw attr =
+  { attr with Unix.c_brkint = false; c_csize = 8; c_echo = false;
+              c_echonl = false; c_icanon = false; c_icrnl = false;
+              c_ignbrk = false; c_igncr = false; c_inlcr = false;
+              c_isig = false; c_istrip = false; c_ixon = false;
+              c_opost = false; c_parenb = false; c_parmrk = false }
+
+let close = Unix.close
+
+let open_port path =
+  let fd = Unix.(openfile path [O_NONBLOCK; O_RDWR] 0o640) in
+  Unix.(tcflush fd TCIOFLUSH);
+  (* TODO: Set speed *)
+  Unix.(tcsetattr fd TCSANOW (cfmakeraw (tcgetattr fd)));
+  fd
+
+let timeout = 5.0
+
+let read fd n =
+  let buf = Bytes.make n '\x00' in
+  let rec aux k =
+    if k < n then
+      match Unix.select [fd] [] [] timeout with
+      | [fd], [], [] ->
+         let m = Unix.read fd buf k (n - k) in
+         aux (k + m)
+      | _ -> raise Timeout
+    else
+      buf in
+  aux 0
+
+let write fd buf =
+  let n = Bytes.length buf in
+  let rec aux k =
+    if k < n then
+      match Unix.select [] [fd] [] timeout with
+      | [], [fd], [] ->
+         let m = Unix.write fd buf k (n - k) in
+         aux (k + m)
+      | _ -> raise Timeout in
+  aux 0
---
+++ ser_port.mli
@@ -0,0 +1,19 @@
+(** Serial port connection to the docking station. *)
+
+(** Serial port state. *)
+type t
+
+(** Operation is timed out *)
+exception Timeout
+
+(** Close serial port. *)
+val close : t -> unit
+
+(** Open serial device file at [path]. *)
+val open_port : string -> t
+
+(** Read exactly [n] bytes from the port, or fail with [Timeout]. *)
+val read : t -> int -> bytes
+
+(** Write data from [buf] to the port, or fail with [Timeout]. *)
+val write : t -> bytes -> unit
