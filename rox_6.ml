06cdb677e39f3d449a0260d95a66cceb148d1e89
Anton Yabchinskiy <arn@bestmx.ru>
2015-02-11 00:10:14 +0300

    Rox_6: Log_summary isn't needed in Log.scan

--- rox_6.ml
+++ rox_6.ml
@@ -278,7 +278,7 @@ module Log =
         marker : Log_marker.t list;
       }
 
-    let scan summary buf =
+    let scan buf =
       let n = String.length buf in
       let rec aux k ans =
         if k < n then
@@ -567,8 +567,8 @@ module Totals =
 let log_summary =
   Log_summary.scan % command ~code:0xEF ~address:0x0071 ~ans_size:53
 
-let log port ({ Log_summary.log_size; _ } as summary) =
-  package_command port ~code:0xEF ~address:log_address ~ans_size:log_size |> Log.scan summary
+let log port { Log_summary.log_size; _ } =
+  package_command port ~code:0xEF ~address:log_address ~ans_size:log_size |> Log.scan
 
 let bat_low =
   Bat_low.scan % command ~code:0xEF ~address:0x006A ~ans_size:7
