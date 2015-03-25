b911dac56426e40ad9fb545596c211a0b42ebfa7
Anton Yabchinskiy <arn@bestmx.ru>
2015-03-26 00:40:23 +0300

    Rox6.Log_summary: restore module, can't be separate due to circ deps

--- rox6.mli
+++ rox6.mli
@@ -76,7 +76,52 @@ sig
   type t = Log_entry.t list
 end
 
-module Log_summary : (module type of Rox6_log_summary)
+module Log_summary :
+sig
+  type t = {
+      start_date : Date.t;        (**  *)
+      start_time : Time.t;
+
+      (** Rider's age (years). *)
+      age : int;
+      (** Rider's body mass (kilograms). *)
+      mass : float;
+      (** Rider's sex. *)
+      sex : Sex.t;
+
+      (** Rider's maximum heart rate (beats per minute). *)
+      max_hr : int;
+      (** Lower and upper heart rate limits of rider's own training zone
+       * (beats per minute). *)
+      hr_limits : int * int;
+
+      training_zone : Training_zone.t;
+      zone_start : float * float * float * float; (* frac of max_hr *)
+
+      bike_no : Bike_no.t;
+      wheel_circum : float;     (* m *)
+
+      distance : float;         (* m *)
+      duration : int;           (* s *)
+      max_speed : float * int;  (* km/h, log entry index *)
+      alt_gain : float;         (* m*)
+      alt_loss : float;         (* m *)
+      kcal : int;               (* kcal *)
+
+      hike_duration : int;      (* s *)
+      hike_alt_gain : float;    (* m *)
+      hike_alt_loss : float;    (* m *)
+      hike_kcal : int;          (* kcal *)
+
+      speed_unit : Speed_unit.t;
+      mass_unit : Mass_unit.t;
+
+      log_size : int;        (* size of log in device memory *)
+    }
+
+  val recv : Ser_port.t -> t
+end
+
 module Settings : (module type of Rox6_settings)
 module Totals : (module type of Rox6_totals)
 
