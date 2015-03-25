70a93d2d8a4c72f469ca24b717b6fc1813ed5d10
Anton Yabchinskiy <arn@bestmx.ru>
2015-03-25 13:48:57 +0300

    Rox_6.Bike_pause: reduce pause entry duration if prev entry's also from pause

--- rox_6.ml
+++ rox_6.ml
@@ -331,7 +331,15 @@ module Bike_pause =
         else
           let tmp_entry =
             Bike_entry.scan wheel_circum prev_entry buf in
-          Bike_entry.Entry (Bike_entry.fill_ts prev_entry {tmp_entry with Bike_entry.duration}) in
+          Bike_entry.Entry (
+              Bike_entry.fill_ts
+                prev_entry {tmp_entry with
+                             Bike_entry.duration =
+                               duration -
+                                 (match prev_entry with
+                                    Bike_entry.Pause_entry e -> e.Bike_entry.duration
+                                  | Bike_entry.No_entry | Bike_entry.Entry _ -> 0)}
+            ) in
       let pause =
         { ts = duration +
                  (match prev_entry with
