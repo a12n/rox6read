88f5baab6933c44313613eef7147abe3a704fac6
Anton Yabchinskiy <arn@bestmx.ru>
2015-03-25 23:43:23 +0300

    Rox_6: settings are in separate module

--- rox_6.mli
+++ rox_6.mli
@@ -76,49 +76,11 @@ sig
   type t = Log_entry.t list
 end
 
-module Settings :
-sig
-  type t = {
-      (* Person *)
-      age : int;                (* y *)
-      mass : float;             (* kg *)
-      sex : Sex.t;
-      (* Heart rate *)
-      max_hr : int;             (* bpm *)
-      hr_limits : int * int;    (* bpm *)
-      (* Training zones *)
-      training_zone : Training_zone.t;
-      zone_alarm : bool;
-      zone_start : float * float * float * float; (* frac of max_hr *)
-      (* Bike *)
-      wheel_circum : float * float; (* m *)
-      (* Date and time *)
-      date : Date.t;
-      time : Time.t;
-      (* Altitude *)
-      slp : int;                (* Pa *)
-      actual_alt : float;       (* m *)
-      home_alt : float;         (* m *)
-      alt_ref : Alt_ref.t;
-      (* Device *)
-      lang : Lang.t;
-      date_format : Date_format.t;
-      speed_unit : Speed_unit.t;
-      mass_unit : Mass_unit.t;
-      contrast : Contrast.t;
-      low_bat : Low_bat.t;
-      (* Service interval *)
-      serv_interval : bool * int; (* km *)
-    }
-end
-
 exception Invalid_response of string
 
 val log : Ser_port.t -> Rox_6_log_summary.t -> Log.t
 
 val bat_low : Ser_port.t -> bool
-
-val settings : Ser_port.t -> Settings.t
 
 val log_addr : int
 
